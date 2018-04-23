9807911ca04b6090f3fcac2de2612cbb61734a82
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/hottrackchart/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace hottrackchart {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.productsTableAdapter = new hottrackchart.nwindDataSetTableAdapters.ProductsTableAdapter();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nwindDataSet = new hottrackchart.nwindDataSet();
+            this.toolTipController1 = new DevExpress.Utils.ToolTipController(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.CrosshairEnabled = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl1.DataAdapter = this.productsTableAdapter;
+            this.chartControl1.DataSource = this.productsBindingSource;
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Name = "chartControl1";
+            series1.ArgumentDataMember = "ProductName";
+            series1.CrosshairEnabled = DevExpress.Utils.DefaultBoolean.False;
+            series1.DataFilters.ClearAndAddRange(new DevExpress.XtraCharts.DataFilter[] {
+            new DevExpress.XtraCharts.DataFilter("CategoryID", "System.Int32", DevExpress.XtraCharts.DataFilterCondition.Equal, 4)});
+            series1.Name = "Series 1";
+            series1.ToolTipEnabled = DevExpress.Utils.DefaultBoolean.False;
+            series1.ValueDataMembersSerializable = "UnitPrice";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.SeriesTemplate.CrosshairEnabled = DevExpress.Utils.DefaultBoolean.True;
+            this.chartControl1.Size = new System.Drawing.Size(584, 387);
+            this.chartControl1.TabIndex = 0;
+            this.chartControl1.ToolTipController = this.toolTipController1;
+            this.chartControl1.ToolTipEnabled = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl1.ObjectHotTracked += new DevExpress.XtraCharts.HotTrackEventHandler(this.chartControl1_ObjectHotTracked);
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.nwindDataSet;
+            // 
+            // nwindDataSet
+            // 
+            this.nwindDataSet.DataSetName = "nwindDataSet";
+            this.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 387);
+            this.Controls.Add(this.chartControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private nwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private nwindDataSet nwindDataSet;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private DevExpress.Utils.ToolTipController toolTipController1;
+    }
+}
+
---
+++ CS/hottrackchart/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+
+namespace hottrackchart {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.nwindDataSet.Products);
+        }
+
+        private void chartControl1_ObjectHotTracked(object sender, DevExpress.XtraCharts.HotTrackEventArgs e) {
+            SeriesPoint point = e.AdditionalObject as SeriesPoint;           
+            
+            if (point != null) {
+                DataRowView rowView = (DataRowView)point.Tag;
+
+                string s = "Unit price = " + rowView["UnitPrice"].ToString() +
+                    "\r\nUnits in stock = " + rowView["UnitsInStock"].ToString() +
+                    "\r\nQuantity per unit = " + rowView["QuantityPerUnit"].ToString();              
+                
+                toolTipController1.ShowHint(s);
+            }
+            else
+             toolTipController1.HideHint();
+        }
+    }
+}
