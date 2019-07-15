8582d9bb2ff9fa731c2ede21f83f24b438fdb861
DevExpressExampleBot <fakeEmail>
2019-07-15 13:52:18 +0300

    Source auto update [skip ci]

---
+++ VB/CustomInfoInTooltips/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Windows.Forms
+Namespace CustomInfoInTooltips
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+			' TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
+			Me.productsTableAdapter.Fill(Me.nwindDataSet.Products)
+			chartControl1.Series(0).CrosshairLabelPattern = "Unit price: {UnitPrice}" & vbCrLf &
+															"Units in stock: {UnitsInStock}" & vbCrLf &
+															"Quantity per unit: {QuantityPerUnit}"
+		End Sub
+	End Class
+End Namespace
