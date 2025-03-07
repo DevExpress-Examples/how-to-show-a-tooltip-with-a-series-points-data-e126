df05bab1e9f08a7aeb8094ef30e888bc00b62c44
DevExpressExampleBot <fakeEmail>
2025-03-07 20:09:53 +0400

    Source auto update [skip ci]

--- VB/CustomInfoInTooltips/Form1.vb
+++ VB/CustomInfoInTooltips/Form1.vb
@@ -1,18 +1,19 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
-Namespace CustomInfoInTooltips
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			' TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
-			Me.productsTableAdapter.Fill(Me.nwindDataSet.Products)
-			chartControl1.Series(0).CrosshairLabelPattern = "Unit price: {UnitPrice}" & vbCrLf &
-															"Units in stock: {UnitsInStock}" & vbCrLf &
-															"Quantity per unit: {QuantityPerUnit}"
-		End Sub
-	End Class
+Namespace CustomInfoInTooltips
+
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
+            productsTableAdapter.Fill(nwindDataSet.Products)
+            chartControl1.Series(0).CrosshairLabelPattern = "Unit price: {UnitPrice}" & Microsoft.VisualBasic.Constants.vbCrLf & "Units in stock: {UnitsInStock}" & Microsoft.VisualBasic.Constants.vbCrLf & "Quantity per unit: {QuantityPerUnit}"
+        End Sub
+    End Class
 End Namespace
